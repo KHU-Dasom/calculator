6581e235040f148ea3f68a12500288b269a06f7b
gunheui <pgh5986@gmail.com>
2021-04-15 22:21:57 +0900

    숫자 버튼 맵핑

--- src/Calc.js
+++ src/Calc.js
@@ -8,24 +8,7 @@ function Calc({ setHistories }) {
       <div className="calc__keyboard">
         <div className="calc__keyboard__main">
           <div className="button calc__keyboard__ac">AC</div>
-          <div class="calc__keyboard__number">
-            <div class="calc__keyboard__number__line">
-              <div class="button button--blue">1</div>
-              <div class="button button--blue">2</div>
-              <div class="button button--blue">3</div>
-            </div>
-            <div class="calc__keyboard__number__line">
-              <div class="button button--blue">4</div>
-              <div class="button button--blue">5</div>
-              <div class="button button--blue">6</div>
-            </div>
-            <div class="calc__keyboard__number__line">
-              <div class="button button--blue">7</div>
-              <div class="button button--blue">8</div>
-              <div class="button button--blue">9</div>
-            </div>
-          </div>
-          <div className="button button--blue--big">0</div>
+          <PadForNumber />
         </div>
         <div className="calc__keyboard__operator">
           <div className="button button--deepblue">+</div>
@@ -37,6 +20,36 @@ function Calc({ setHistories }) {
       </div>
     </div>
   );
+}
+
+function Calcbutton({ value, color }) {
+  return <div className={`button button--${color}`}>{value}</div>;
+}
+
+function PadForNumber() {
+  const NumberLine = ({ idx }) => {
+    idx = idx * 3 + 1;
+    const buttonIdx = [idx, idx + 1, idx + 2];
+    const numberLine = buttonIdx.map((value) => (
+      <Calcbutton color="blue" value={value} />
+    ));
+    return <div className="calc__keyboard__number__line">{numberLine}</div>;
+  };
+
+  const NumberPad = () => {
+    const numberLineidx = Array(3).fill(null);
+    const numberPad = numberLineidx.map((value, idx) => (
+      <NumberLine idx={idx} />
+    ));
+    numberPad.reverse();
+    return (
+      <div class="calc__keyboard__number">
+        {numberPad}
+        <Calcbutton color="blue--big" value="0" />
+      </div>
+    );
+  };
+  return <NumberPad />;
 }
 
 export default Calc;
