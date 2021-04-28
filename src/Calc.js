a409e074681290e5398ca04a7f61e385487176ff
juicyorange <rlaxodud9809@khu.ac.kr>
2021-04-28 16:35:54 +0900

    Add: 계산기능구현
    컴포넌트 추후 정리 필요

--- src/Calc.js
+++ src/Calc.js
@@ -1,38 +1,149 @@
-import React from "react";
+import React, { useState } from 'react';
+import History from './History';
+
+const numbers = [
+  ['1', '2', '3'],
+  ['4', '5', '6'],
+  ['7', '8', '9'],
+];
+
+const sings = ['+', '-', 'x', '÷', '='];
 
 function Calc({ setHistories }) {
+  // 관리해야 할 것.
+  // 현재입력, 이전까지 계산된 것, 부호기억
+  const [numInput, setNumInput] = useState('0');
+  const [sign, setSign] = useState('');
+  const [amount, setAmount] = useState('0');
+
+  // 총 결과를 보여줄지, 아니면 현재 입력하는 숫자를 보여줄지
+  // 입력하는 숫자를 보여준다면 true, 총 결과면 false
+  const [valid, setValid] = useState(true);
+
+  const onClickNum = (e) => {
+    const value = e.target.value;
+    if (numInput === '0') {
+      setNumInput(value);
+      setValid(true);
+    } else {
+      setNumInput((prevValue) => prevValue + value);
+      setValid(true);
+    }
+  };
+
+  const onClickSign = (e) => {
+    const inputSign = e.target.value;
+
+    if (inputSign === 'ac') {
+      setAmount('0');
+      setNumInput('0');
+      setSign('');
+      setValid(false);
+    } else {
+      switch (sign) {
+        case '':
+          setAmount(numInput);
+          setNumInput('0');
+          if (inputSign === '=') setSign('');
+          else setSign(inputSign);
+          setValid(false);
+          break;
+        case '+':
+          setAmount((prevValue) =>
+            String(Number(prevValue) + Number(numInput)),
+          );
+          setNumInput('0');
+          if (inputSign === '=') setSign('+');
+          else setSign(inputSign);
+          setValid(false);
+          break;
+        case '-':
+          setAmount((prevValue) =>
+            String(Number(prevValue) - Number(numInput)),
+          );
+          setNumInput('0');
+          if (inputSign === '=') setSign('-');
+          else setSign(inputSign);
+          setValid(false);
+          break;
+        case 'x':
+          setAmount((prevValue) =>
+            String(Number(prevValue) * Number(numInput)),
+          );
+          if (inputSign === '=') {
+            setSign('x');
+            setNumInput('1');
+          } else {
+            setSign(inputSign);
+            setNumInput('0');
+          }
+          setValid(false);
+          break;
+        case '÷':
+          setAmount((prevValue) =>
+            String(Number(prevValue) / Number(numInput)),
+          );
+          if (inputSign === '=') {
+            setSign('x');
+            setNumInput('1');
+          } else {
+            setSign(inputSign);
+            setNumInput('0');
+          }
+          setValid(false);
+          break;
+        default:
+          break;
+      }
+    }
+  };
+
   return (
     <div className="calc">
       <div className="calc__title">Calculator</div>
-      <div className="calc__score">0</div>
+      <div className="calc__score">{valid ? numInput : amount}</div>
       <div className="calc__keyboard">
         <div className="calc__keyboard__main">
-          <div className="button calc__keyboard__ac">AC</div>
-          <div class="calc__keyboard__number">
-            <div class="calc__keyboard__number__line">
-              <div class="button button--blue">1</div>
-              <div class="button button--blue">2</div>
-              <div class="button button--blue">3</div>
-            </div>
-            <div class="calc__keyboard__number__line">
-              <div class="button button--blue">4</div>
-              <div class="button button--blue">5</div>
-              <div class="button button--blue">6</div>
-            </div>
-            <div class="calc__keyboard__number__line">
-              <div class="button button--blue">7</div>
-              <div class="button button--blue">8</div>
-              <div class="button button--blue">9</div>
-            </div>
+          <button
+            className="button calc__keyboard__ac"
+            value="ac"
+            onClick={onClickSign}
+          >
+            AC
+          </button>
+          <div className="calc__keyboard__number">
+            {numbers.map((line) => (
+              <div class="calc__keyboard__number__line">
+                {line.map((num) => (
+                  <button
+                    className="button button--blue"
+                    value={num}
+                    onClick={onClickNum}
+                  >
+                    {num}
+                  </button>
+                ))}{' '}
+              </div>
+            ))}
           </div>
-          <div className="button button--blue--big">0</div>
+          <button
+            className="button button--blue--big"
+            value={0}
+            onClick={onClickNum}
+          >
+            0
+          </button>
         </div>
         <div className="calc__keyboard__operator">
-          <div className="button button--deepblue">+</div>
-          <div className="button button--deepblue">-</div>
-          <div className="button button--deepblue">×</div>
-          <div className="button button--deepblue">÷</div>
-          <div className="button button--pink">=</div>
+          {sings.map((sign) => (
+            <button
+              className="button button--deepblue"
+              value={sign}
+              onClick={onClickSign}
+            >
+              {sign}
+            </button>
+          ))}
         </div>
       </div>
     </div>
